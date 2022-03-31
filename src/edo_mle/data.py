7abe68baea499aaff4f15e2815ff2dacf06fbc93
Peter Foley <peter.foley@edoinc.com>
2022-03-31 14:46:12 -0700

    linting

--- src/edo_mle/data.py
+++ src/edo_mle/data.py
@@ -1,20 +1,22 @@
 from functools import cache
-from typing import Dict, List, Set, Tuple, Iterator, TypeVar
-import pandas as pd
+from typing import Dict, Set, Tuple, TypeVar
+
 import numpy as np
 import numpy.typing as npt
+import pandas as pd
+
 
 @cache
 def get_iris() -> pd.DataFrame:
-    csv_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+    csv_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
     cols = {
-        'sepal_length':np.float32,
-        'sepal_width':np.float32,
-        'petal_length':np.float32,
-        'petal_width':np.float32,
-        'species':pd.CategoricalDtype(),
+        "sepal_length": np.float32,
+        "sepal_width": np.float32,
+        "petal_length": np.float32,
+        "petal_width": np.float32,
+        "species": pd.CategoricalDtype(),
     }
-    iris = pd.read_csv(csv_url, names = list(cols), dtype=cols)
+    iris = pd.read_csv(csv_url, names=list(cols), dtype=cols)
     iris["orig_row"] = np.arange(len(iris))
     return iris
 
@@ -22,11 +24,11 @@ def get_iris() -> pd.DataFrame:
 def xy_species(iris=None) -> Tuple[pd.DataFrame, pd.Series]:
     iris = iris if iris is not None else get_iris()
     target = iris["species"]
-    data = iris.drop("species",axis=1)
+    data = iris.drop("species", axis=1)
     return data, target
 
 
-def cv_splits(n,k,seed):
+def cv_splits(n, k, seed):
     assignments = np.arange(n) % k
     randomizer = np.random.default_rng(seed)
     randomizer.shuffle(assignments)
@@ -35,13 +37,17 @@ def cv_splits(n,k,seed):
 
 XYDF = Tuple[pd.DataFrame, pd.Series]
 IntArray = npt.NDArray[np.int_]
-T = TypeVar('T')
-def extract_splits(xy:XYDF, n_splits:int, this_split:Dict[T, Set[int]]) -> Dict[T, Tuple[XYDF, XYDF]]:
+T = TypeVar("T")
+
+
+def extract_splits(
+    xy: XYDF, n_splits: int, this_split: Dict[T, Set[int]]
+) -> Dict[T, Tuple[XYDF, XYDF]]:
     """
     Generator that returns (x,y) tuples for each cv split.
     where cv splits are defined by a dictionary that tells the indices to keep
     """
-    x,y = xy
+    x, y = xy
     i = cv_splits(len(x), n_splits, 1234)
     results: Dict[T, Tuple[XYDF, XYDF]] = dict()
     for label, set_of_i_to_keep in this_split.items():
@@ -50,4 +56,3 @@ def extract_splits(xy:XYDF, n_splits:int, this_split:Dict[T, Set[int]]) -> Dict[
         kept_y = y[should_keep_row]
         results[label] = (kept_x, kept_y)
     return results
-
--- src/edo_mle/files.py
+++ src/edo_mle/files.py
@@ -1,33 +1,37 @@
-from airflow.operators.python import get_current_context
-from airflow.configuration import get_airflow_home
 import os
-from pprint import pprint
-import pandas as pd
 import pickle
+
+from airflow.configuration import get_airflow_home
+from airflow.operators.python import get_current_context
+
 
 def root_dir():
     home = get_airflow_home()
     return f"{home}/data"
+
 
 def run_dir():
     context = get_current_context()
     run_id = context["run_id"]
     return f"{root_dir()}/{run_id}"
 
+
 def task_dir():
     context = get_current_context()
     task_key = context["task_instance_key_str"]
     return f"{root_dir()}/{task_key}"
 
+
 def save(x, name):
     dirname = task_dir()
     os.makedirs(dirname, exist_ok=True)
     filename = f"{dirname}/{name}.pkl"
-    with open(filename, 'wb') as f:
+    with open(filename, "wb") as f:
         pickle.dump(x, f)
     return filename
 
+
 def load(path):
-    with open(path, 'rb') as f:
+    with open(path, "rb") as f:
         res = pickle.load(f)
     return res
